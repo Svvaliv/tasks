9a33e9f33e6dcc23b8323361918dd8f064cbfb81
Valeriy Svetkin <svvaliv@gmail.com>
2022-07-26 21:23:01 +0500

    добавил условие проверки даты

--- svvaliv/exam_results.py
+++ svvaliv/exam_results.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from datetime import datetime
 
 with open('data/exam_results.csv', encoding='utf-8') as fi, \
         open('data/best_scores.json', 'w', encoding='utf-8') as fo:
@@ -15,10 +16,15 @@ with open('data/exam_results.csv', encoding='utf-8') as fi, \
         }
         key = student['name'] + ' ' + student['surname']
         if key in students_out:
-            if int(student['score']) >= int(students_out[key]['best_score']):
+            now_date = datetime.fromisoformat(student['date_and_time'])
+            exam_date = datetime.fromisoformat(students_out[key]['date_and_time'])
+            now_score, exam_score = int(student['score']), int(students_out[key]['best_score'])
+            if now_score > exam_score:
                 students_out[key]['best_score'] = int(student['score'])
+                students_out[key]['date_and_time'] = student['date_and_time']
+            elif now_score == exam_score and now_date > exam_date:
                 students_out[key]['date_and_time'] = student['date_and_time']
         else:
             students_out[key] = student_info
-    students_out = [info for info in sorted(students_out.values(), key=lambda item: item['email'].lower())]
+    students_out = [info for info in sorted(students_out.values(), key=lambda item: item['email'])]
     json.dump(students_out, fo, indent=3)
