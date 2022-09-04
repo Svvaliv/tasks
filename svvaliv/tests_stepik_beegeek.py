523c5b6beb4ed60514ea6db6aa78e73bb0af2ce1
Valeriy Svetkin <svvaliv@gmail.com>
2022-09-04 11:20:38 +0500

    добавил тестирование регулярок

--- svvaliv/tests_stepik_beegeek.py
+++ svvaliv/tests_stepik_beegeek.py
@@ -14,8 +14,7 @@
 '''
 
 def testing_lesson(filename: str):
-    '''Функция распаковывет архив с тестовыми данными, затем выполняет код из каждого файла и выводит
-    результат выполнения вашего кода и ожидаемый результат'''
+    '''Функция выполняет код из каждого файла и выводит результат выполнения вашего кода и ожидаемый результат'''
     from zipfile import ZipFile
 
     with ZipFile(filename) as z:
@@ -39,3 +38,32 @@ def testing_lesson(filename: str):
 
 filename = 'data/tests/tests_2777710.zip'
 testing_lesson(filename)
+
+def testing_lesson_regex(filename: str, regex):
+    '''Функция применяет регулярное выражение к строке из файла, а потом сравнивает ваш результат и результат вывода'''
+    from zipfile import ZipFile
+    import re
+
+    flag = 'OK'
+    with ZipFile(filename) as z:
+        files = [(code, out) for code, out in zip(z.namelist()[::2], z.namelist()[1::2])]
+        for file_exec, file_out in files:
+            with z.open(name=file_exec) as fi:
+                with z.open(name=file_out) as fo:
+                    string, result = fi.read().decode(), fo.read().decode()
+                    try:
+                        f = re.finditer(regex, string)
+                        my_result = ' '.join([item.group() for item in f])
+                        if my_result != result:
+                            flag = 'Failed'
+                        print(f'Тест № {file_exec}: {flag}')
+                        print(f'Результат: {my_result}')
+                        print(f'Ожидаемый результат: {result}')
+                        print()
+                    except Exception as e:
+                        print(f'{"🚫" * 50}\n"Тест № {file_exec} завершился с `ошибкой "{type(e).__name__}: {e}\n')
+                        print(f'Ожидаемый результат: \n{result}\n{"🚫" * 50}"\n')
+
+regex = r''
+filename = 'data/tests/tests_2862306.zip'
+testing_lesson_regex(filename, regex)
